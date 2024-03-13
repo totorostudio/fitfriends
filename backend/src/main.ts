264aa4bc822355e159235ed44336a48d038851b5
Andrew Stepanov <stepanov.management@gmail.com>
2024-03-13 14:17:55 +0500

    Создан модуль user. Сделана документация через Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PORT } from './app.const';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fit Friends')
+    .setDescription('Fit Friends API description')
+    .setVersion('1.0')
+    .addTag('trainings')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT, () => {
     new Logger('Bootstrap').log(`🚀 Started on http://localhost:${PORT}/`);
   });
