282204d426e8a9509884b49edd9a0b8ad506482e
Andrew Stepanov <stepanov.management@gmail.com>
2024-04-26 11:13:49 +0500

    Доработан модуль Subscribe

--- backend/src/modules/subscribe/subscribe.controller.ts
+++ backend/src/modules/subscribe/subscribe.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Param, Delete, HttpStatus, Req, UseGuards } from '@nestjs/common';
 import { SubscribeService } from './subscribe.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Public, Role } from 'src/libs/decorators';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Role } from 'src/libs/decorators';
 import { UserRole } from 'src/libs/types';
 import { UUIDValidationPipe } from 'src/libs/pipes';
 import { RoleGuard } from 'src/libs/guards';
@@ -12,11 +12,16 @@ import { RequestWithTokenPayload } from 'src/libs/requests';
 export class SubscribeController {
   constructor(private readonly subscribeService: SubscribeService) {}
 
+  @ApiOperation({
+    summary: 'Подписаться на уведомления о новых тренировках'
+  })
   @ApiResponse({
     status: HttpStatus.CREATED,
     description: 'Подписка на уведомления о новых тренировках тренера успешно добавлена',
   })
   @Role(UserRole.Customer)
+  @UseGuards(RoleGuard)
+  @ApiBearerAuth('access-token')
   @Post(':coachId')
   public async create(
     @Param('coachId', UUIDValidationPipe) coachId: string,
@@ -25,12 +30,16 @@ export class SubscribeController {
     await this.subscribeService.addNewSubscription(tokenPayload, coachId);
   }
 
+  @ApiOperation({
+    summary: 'Отписаться от уведомлений о новых тренировках'
+  })
   @ApiResponse({
     status: HttpStatus.OK,
     description: 'Подписка успешно удалена',
   })
   @Role(UserRole.Customer)
   @UseGuards(RoleGuard)
+  @ApiBearerAuth('access-token')
   @Delete(':coachId')
   public async delete(
     @Param('coachId', UUIDValidationPipe) coachId: string,
